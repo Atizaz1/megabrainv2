34304aaab2bb83a9f5ff1d41439af162ef910fc7
Atizaz Ahmad <atizaz.ahmad07@gmail.com>
2020-05-18 11:52:08 +0500

    Full Screen Image Adjustment

--- lib/Screens/solo_image_screen.dart
+++ lib/Screens/solo_image_screen.dart
@@ -41,55 +41,53 @@ class _SoloImageScreenState extends State<SoloImageScreen>
   @override
   Widget build(BuildContext context) 
   {
-    return SafeArea(
-          child: Scaffold(
-          body: Container(
-          height: MediaQuery.of(context).size.height / 1,
-          child: PhotoView.customChild(
-                child: Container(
-                decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: imgLink != null ? CachedNetworkImageProvider(imgLink):
-                  AssetImage('assets/images/default_image.jpg'),
-                  fit: BoxFit.cover
-                ),
-              ),
-              child: imgLink == null ? Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  SizedBox(height:80),
-                  Center(
-                    // heightFactor: 20,
-                    child: Container(
-                      // margin: EdgeInsets.all(20),
-                      child:InkWell(
-                      onTap:()
-                      {
-                        print('Hello');
-                        StoreRedirect.redirect(
-                              androidAppId: "com.monday.monday",
-                              iOSAppId: "1290128888");
-                      },
-                      child: Container(
-                        width: 230,
-                        height: 230,
-                        decoration:BoxDecoration(
-                          image: DecorationImage(
-                            image: AssetImage('assets/images/playstore2.png'),
-                            fit:BoxFit.contain
-                            )
-                        ),
-                        ),
-                        ),
-                    ),
-                  ),
-                ],
-              ):
-              Container()
-            ),
+    return Scaffold(
+    body: Container(
+    height: MediaQuery.of(context).size.height / 1,
+    child: PhotoView.customChild(
+          child: Container(
+          decoration: BoxDecoration(
+          image: DecorationImage(
+            image: imgLink != null ? CachedNetworkImageProvider(imgLink):
+            AssetImage('assets/images/default_image.jpg'),
+            fit: BoxFit.contain
           ),
-       ),
+        ),
+        child: imgLink == null ? Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(height:80),
+            Center(
+              // heightFactor: 20,
+              child: Container(
+                // margin: EdgeInsets.all(20),
+                child:InkWell(
+                onTap:()
+                {
+                  print('Hello');
+                  StoreRedirect.redirect(
+                        androidAppId: "com.monday.monday",
+                        iOSAppId: "1290128888");
+                },
+                child: Container(
+                  width: 230,
+                  height: 230,
+                  decoration:BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/images/playstore2.png'),
+                      fit:BoxFit.contain
+                      )
+                  ),
+                  ),
+                  ),
+              ),
+            ),
+          ],
+        ):
+        Container()
       ),
-    );
+    ),
+       ),
+      );
   }
 }
